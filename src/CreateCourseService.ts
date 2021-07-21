3bbf3406e5d2237d225f6a7f924a9198f259f0f9
WillianFarias <williansousafarias@gmail.com>
2021-07-21 16:04:26 -0300

    metodo execute

--- src/CreateCourseService.ts
+++ src/CreateCourseService.ts
@@ -6,5 +6,9 @@
 
 class CreateCourseService {
   
-  execute()
+  execute(name: string, duration: number, educator: string) {
+    console.log(name, duration, educator);
+  }
 }
+
+export default new CreateCourseService();
