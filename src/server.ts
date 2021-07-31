ce092fda613097b01c272b4a04f0494d8f60768d
WillianFarias <williansousafarias@gmail.com>
2021-07-31 17:21:58 -0300

    adicionando ts-node-dev ao projeto e criando script

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,5 @@
-import express, { request, response } from 'express';
-import { createCourse } from './routes';
+import express from "express";
 
 const app = express();
 
-// app.get("/", (request, response) => {
-//   return response.json({ message: "Hello World!" });
-// })
-
-app.get("/", createCourse);
-
-app.listen(3333);
+app.listen(3333, () => console.log("Server is running!"));
